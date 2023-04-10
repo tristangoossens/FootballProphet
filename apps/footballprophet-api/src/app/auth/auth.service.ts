b7ae83c7613d91d137213b4030e77362a266e8f2
Tristan Goossens <tt.goossens@student.avans.nl>
2023-04-10 14:33:14 +0200

    added error handling

--- apps/footballprophet-api/src/app/auth/auth.service.ts
+++ apps/footballprophet-api/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@footballprophet/domain';
@@ -12,20 +12,42 @@ export class AuthService {
   ) {}
 
   async validateUser(username: string, pass: string): Promise<User> {
-    const user = await this.userService.findByUsername(username);
-    const valid = await bcrypt.compare(pass, user.password);
+    try {
+      const user = await this.userService.findByUsername(username);
+      const valid = await bcrypt.compare(pass, user.password);
 
-    if (valid) {
-      delete user.password;
-      return user;
+      if (valid) {
+        delete user.password;
+        return user;
+      } else {
+        throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+      }
+    } catch (error) {
+      Logger.error(
+        `⚠️: Something went wrong (AuthService -> validateUser): ${error.message}`
+      );
+
+      throw new HttpException(
+        'Log in failed, please try again',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
     }
-
-    return null;
   }
 
   async login(user: any) {
-    return {
-      access_token: this.jwtService.sign(user.toJSON()),
-    };
+    try {
+      return {
+        access_token: this.jwtService.sign(user.toJSON()),
+      };
+    } catch (error) {
+      Logger.error(
+        `⚠️: Something went wrong (AuthService -> login): ${error.message}`
+      );
+
+      throw new HttpException(
+        'Something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 }
